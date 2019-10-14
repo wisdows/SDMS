f0acc8765263c17cf5d4dff26abfc2ec1a016eda
wisdows <2929903706@qq.com>
2019-10-14 23:57:00 +0800

    Initial commit

--- src/MainClass.java
+++ src/MainClass.java
@@ -5,6 +5,7 @@ public class MainClass {
 		StudentDao dao = new StudentDao();// 创建一个dao对象
 		List<StudentBean> list = dao.findAll();
 		
+		
 		System.out.println("查询结果,共有"+list.size()+"条记录");
 		for(StudentBean s:list){
 			System.out.println(s.getName());
